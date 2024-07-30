a73c72ac34054e592bad8053fea9a975bea520e2
Seven <ahmadaliev.sd@gmail.com>
2024-07-30 14:50:55 +0900

    header section completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/Signin'
+import SignUp from './pages/Signup'
+import Header from './components/Header'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-        
-    </>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
