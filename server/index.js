f790f4bf5d7e70a3215780449e048f3538f2023c
Seven <ahmadaliev.sd@gmail.com>
2024-07-30 22:48:42 +0900

    signup api created, password hashing etc.

--- server/index.js
+++ server/index.js
@@ -1,18 +1,39 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
-dotenv.config()
+// Load environment variables
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(
-    ()=>{
-        console.log('Connected to MongoDB');
-    }).catch((err)=>{
-        console.log(err);
-    });
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
 
 const app = express();
 
+// Middleware to parse JSON
+app.use(express.json());
+
+// Register routes
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.status || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    });
+});
+
+// Start the server
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
