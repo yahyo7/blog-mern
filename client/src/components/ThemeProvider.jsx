8284dc6b0f85b31f7906e17bef7cc0ec32e19987
Seven <ahmadaliev.sd@gmail.com>
2024-08-01 10:42:49 +0900

    profile page completed

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -5,7 +5,7 @@ const ThemeProvider = ({ children }) => {
 
   return (
     <div className={theme}>
-      <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-gray-800 min-h-screen">
+      <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-gray-900 min-h-screen">
         {children}
       </div>
     </div>
