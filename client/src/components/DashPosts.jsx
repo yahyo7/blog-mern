8dc720627568682df735320bfd81c57915457e64
Seven <ahmadaliev.sd@gmail.com>
2024-08-03 10:43:29 +0900

    show more functionality

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -14,6 +14,7 @@ function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
 
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true)
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -24,6 +25,9 @@ function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false)
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -33,6 +37,22 @@ function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length
+    try {
+      const res = await fetch(`/api/post/get-posts?userId=${currentUser._id}&startIndex=${startIndex}`)
+      const data = await res.json()
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts])
+        if (data.posts.length < 9) {
+          setShowMore(false)
+        }
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -96,6 +116,13 @@ function DashPosts() {
               </TableBody>
             ))}
           </Table>
+          {
+            showMore ? (
+              <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">Show more</button>
+            ) : (
+              <p className="text-center text-sm py-7">You have reached the end</p>
+            )
+          }
         </>
       ) : (
         <p>You have no posts yet</p>
