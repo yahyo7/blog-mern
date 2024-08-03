548543b636d3e668241fe461f464969264fa80d0
Seven <ahmadaliev.sd@gmail.com>
2024-08-03 11:08:09 +0900

    delete post functionality

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import {
+  Modal,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableHeadCell,
   TableRow,
+  Button
 } from "flowbite-react";
 import { Link } from "react-router-dom";
+import { HiOutlineExclamationCircle } from "react-icons/hi2";
 
 function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
 
   const [userPosts, setUserPosts] = useState([]);
   const [showMore, setShowMore] = useState(true)
+  const [showModal, setShowModal] = useState(false)
+  const [postIdToDelete, setPostIdToDelete] = useState('')
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -51,6 +56,23 @@ function DashPosts() {
       }
     } catch (error) {
       console.log(error)
+    }
+  }
+
+  const handleDeletePost = async () => {
+    setShowModal(false)
+    try {
+      const res = await fetch(`/api/post/delete-post/${postIdToDelete}/${currentUser._id}`, {
+        method: "DELETE"
+      })
+      const data = await res.json()
+      if (!res.ok) {
+        console.log(data.message)
+      } else {
+        setUserPosts((prev) => prev.filter((post) => post._id !== postIdToDelete))
+      }
+    } catch (error) {
+      
     }
   }
   return (
@@ -101,7 +123,10 @@ function DashPosts() {
                   </TableCell>
                   <TableCell>{post.category}</TableCell>
                   <TableCell>
-                    <span className="font-medium text-red-500 hover:underline cursor-pointer">
+                    <span onClick={()=> {
+                      setShowModal(true)
+                      setPostIdToDelete(post._id)
+                    }} className="font-medium text-red-500 hover:underline cursor-pointer">
                       Delete
                     </span>
                   </TableCell>
@@ -127,6 +152,32 @@ function DashPosts() {
       ) : (
         <p>You have no posts yet</p>
       )}
+
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size="md"
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this post?
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="failure" onClick={handleDeletePost}>
+                Yes, I'm sure
+              </Button>
+              <Button color="gray" onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+
     </div>
   );
 }
--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPost, getPosts } from "../controllers/post.controller.js";
+import { createPost, deletePost, getPosts } from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createPost)
 router.get('/get-posts', getPosts)
+router.delete('/delete-post/:postId/:userId', verifyToken, deletePost)
 
 export default router;
